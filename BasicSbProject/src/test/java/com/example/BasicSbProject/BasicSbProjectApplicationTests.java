c22124b8c297757e07ac89d7960bee92eb75fde5
hemalatp <hemalatha.p@global-csg.com>
2023-03-21 14:11:13 +0530

    first commit

---
+++ BasicSbProject/src/test/java/com/example/BasicSbProject/BasicSbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BasicSbProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
